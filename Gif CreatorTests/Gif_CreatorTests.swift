83bb705de52523b623c24d3b6d39feb943a1efbe
Case Wright <C453@C453.local>
2015-04-23 10:00:34 -0400

    Initial Commit

---
+++ Gif CreatorTests/Gif_CreatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gif_CreatorTests.swift
+//  Gif CreatorTests
+//
+//  Created by Case Wright on 4/23/15.
+//  Copyright (c) 2015 Case Wright. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Gif_CreatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
